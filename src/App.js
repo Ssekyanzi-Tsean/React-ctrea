d3a5a6a9cc36a1a07a17777903fe575aab7b4c8e
Shogun <msean481@gmail.com>
2021-02-13 05:24:40 -0500

    Added The HEader

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+      <h1>Ed's Todo List</h1>
+      </header>
     </div>
   );
 }
